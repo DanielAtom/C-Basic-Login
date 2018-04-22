8ea3c21f13faef67d55ed1c20b2f424018bf615a
DanielBence <38626527+DanielBence@users.noreply.github.com>
2018-04-22 19:27:01 +0300

    Add files via upload

---
+++ Basic Login/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "username" && textBox2.Text == "password")
+            {
+                MessageBox.Show("Logged in.");
+            } else
+            {
+                MessageBox.Show("Incorrect Credentials.");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/DanielBence");
+        }
+    }
+}
